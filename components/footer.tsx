be25a4ad4366c8b354320ecf03da02e3e031c5e5
yamanidev <elamine.yamani@gmail.com>
2024-08-10 00:15:35 +0100

    Made footer responsive

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import { FaInstagram, FaFacebookF, FaLinkedinIn, FaTwitter } from 'react-icons/fa';
+import { OmranLogo } from "@/assets/omran-logo";
+import Link from 'next/link';
+import { FaFacebookF, FaInstagram, FaLinkedinIn, FaTwitter } from 'react-icons/fa';
 import { Button } from './ui/button';
-import { OmranLogo } from "@/assets/omran-logo"
 
 export const Footer = () => {
   return (
     <footer className="bg-black text-white pb-10 pt-20">
       <div className="container mx-auto">
-        <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
+        <div className="grid grid-cols-1 lg:grid-cols-4 gap-8">
           {/* Logo and Social Icons */}
-          <div className="flex flex-col items-center w-fit gap-12">
+          <div className="mx-auto lg:mx-0 flex flex-col items-center w-fit gap-12">
             <div className='w-28 h-28'>
               <OmranLogo width="100%" height="100%" />
             </div>
@@ -22,8 +22,8 @@ export const Footer = () => {
           </div>
 
           {/* Link Columns */}
-          <div className="md:col-span-2 grid grid-cols-3 gap-4">
-            <div className="text-right">
+          <div className="grid grid-cols-2 md:col-span-2 md:grid-cols-3 gap-4">
+            <div>
               <h3 className="font-bold mb-6 text-[1.375rem] leading-[2rem]">الخدمات</h3>
               <ul className="space-y-2 text-white/75">
                 <li>برامجنا</li>
@@ -32,7 +32,7 @@ export const Footer = () => {
                 <li>التدريب</li>
               </ul>
             </div>
-            <div className="text-right">
+            <div>
               <h3 className="font-bold mb-6 text-[1.375rem] leading-[2rem]">الأخبار</h3>
               <ul className="space-y-2 text-white/75">
                 <li>المدونة</li>
@@ -41,7 +41,7 @@ export const Footer = () => {
                 <li>الوظائف</li>
               </ul>
             </div>
-            <div className="text-right">
+            <div className="hidden md:block">
               <h3 className="font-bold mb-6 text-[1.375rem] leading-[2rem]">تواصل</h3>
               <ul className="space-y-2 text-white/75">
                 <li>اتصل بنا</li>
@@ -53,14 +53,41 @@ export const Footer = () => {
             </div>
           </div>
 
+          <div className='grid md:hidden grid-cols-2'>
+            <div>
+              <h3 className="font-bold mb-6 text-[1.375rem] leading-[2rem]">تواصل</h3>
+              <ul className="space-y-2 text-white/75">
+                <li>اتصل بنا</li>
+                <li>أسئلة شائعة</li>
+                <li>الشركاء</li>
+                <li>انضم إلينا</li>
+                <li>ادعمنا</li>
+              </ul>
+            </div>
+            <ul className="space-y-2 text-white/75 underline">
+              <li>
+                <Link href="#" className="">سياسة الخصوصية</Link>
+              </li>
+              <li>
+                <Link href="#">تعريف</Link>
+              </li>
+              <li>
+                <Link href="#">سياسة الكوكيز</Link>
+              </li>
+              <li>
+                <Link href="#">الشروط والأحكام</Link>
+              </li>
+            </ul>
+          </div>
+
           {/* Newsletter Signup */}
-          <div className="md:col-span-1">
+          <div className="md:col-span-1 max-w-xl">
             <h3 className="font-bold mb-6 text-[1.375rem] leading-[2rem]">نحن نبقيك على اطلاع</h3>
             <div className="flex">
               <input
                 type="email"
                 placeholder="الإيميل"
-                className="px-4 h-12 bg-white text-black flex-grow rounded-tr-full rounded-br-full"
+                className="min-w-0 px-4 h-12 bg-white text-black flex-grow rounded-tr-full rounded-br-full"
               />
               <Button className="bg-[#AD9E68] h-12 px-6 text-white rounded-tl-full rounded-bl-full">تسجيل</Button>
             </div>
@@ -68,9 +95,9 @@ export const Footer = () => {
         </div>
 
         {/* Policy Links */}
-        <div className="mt-8 pt-8 border-t border-white/40 text-white grid grid-cols-1 md:grid-cols-2 gap-4 text-xs font-medium">
+        <div className="mt-8 pt-8 border-t border-white/40 text-white lg:grid lg:grid-cols-2 lg:gap-4 text-xs font-medium text-center lg:text-right">
           <p>© 2024<span className='text-[#AD9E68] ms-1'>فعاليات عمران</span>. جميع الحقوق محفوظة</p>
-          <div className="flex flex-wrap justify-center md:justify-end gap-8">
+          <div className="hidden lg:flex flex-wrap justify-center md:justify-end gap-8">
             <a href="#" className="hover:underline mx-2">سياسة الخصوصية</a>
             <a href="#" className="hover:underline mx-2">تعريف</a>
             <a href="#" className="hover:underline mx-2">سياسة الكوكيز</a>
