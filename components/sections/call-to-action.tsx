cbfd82ef390765d3c8d8d8a4dec0866201e1c87c
yamanidev <elamine.yamani@gmail.com>
2024-08-10 02:53:26 +0100

    Section made responsive

--- components/sections/call-to-action.tsx
+++ components/sections/call-to-action.tsx
@@ -5,12 +5,12 @@ import OmranConference from "../../assets/omran-conference.svg";
 
 export const CallToAction = () => {
   return (
-    <section className="w-full py-20 bg-gradient-to-t from-[#252C32] from-15% via-[#708698] via-100% text-white">
+    <section className="w-full lg:py-[4.6875rem] pt-24 pb-[7.25rem] bg-gradient-to-t from-[#252C32] from-15% via-[#708698] via-100% text-white">
       <div className="container mx-auto px-4">
-        <div className="flex flex-col md:flex-row items-center justify-between">
+        <div className="flex flex-col lg:flex-row gap-[7.75rem] lg:gap-0 items-center justify-between">
           <div className="text-right max-w-2xl mb-8 md:mb-0">
             <h2 className="text-4xl leading-tight font-bold mb-6">شراكات فاعلة، مستقبل مزدهر</h2>
-            <p className="text-base font-semibold mb-8">
+            <p className="text-base lg:font-semibold mb-8">
               ندعو رواد الأعمال والمستثمرين إلى خلق أفكار مبتكرة وتطوير مشاريع تساهم
               في بناء مستقبل أفضل. نؤمن بأن المشاريع الناجحة في ذلك هي التي تستطيع
               أن تتجاوز التحديات وتستغل الفرص التي يوفرها الواقع المتغير، وتساهم في
