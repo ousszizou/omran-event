449df4154337f7f24c0b59962d3f2aa52444abf6
oussama djaidri <odjaidri@gmail.com>
2024-08-10 15:54:27 +0100

    complete the timeline

--- components/sections/conference-program.tsx
+++ components/sections/conference-program.tsx
@@ -1,8 +1,34 @@
-import { Separator } from '../ui/separator';
+import React from 'react';
+import { Separator } from "@/components/ui/separator";
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
+
+const TimelineItem = ({ title, presenter, type, isAccordion = false, isFirstInAccordion = false, eventNumber }) => {
+
+  const pointerClasses = isFirstInAccordion
+    ? eventNumber === "02"
+      ? "-top-[0.3rem] -right-[2.73rem]"
+      : eventNumber === "03"
+        ? "-top-[0.3rem] -right-[2.7rem]"
+        : "top-3 -right-3"
+    : "top-3 -right-3";
+
+  return (
+    <div className="relative mb-12">
+      <div className={`absolute w-4 h-4 bg-[#AD9E68] rounded-full ${pointerClasses}`} />
+      <div className={`${eventNumber !== "01" ? 'ps-8' : 'ps-16'}`}>
+        <h4 className="text-[#252C32] text-[1.375rem] leading-[2rem] text-right mb-8 text-balance font-bold">{title}</h4>
+        <div className="flex gap-8 items-center">
+          <div className='text-white rounded-full px-5 py-2 bg-gradient-to-r from-[#AD9E68] from-0% to-[#47412B] to-100% text-xs'>{type}</div>
+          <p className="text-lg text-[#AD9E68] text-right font-medium">{presenter}</p>
+        </div>
+      </div>
+    </div>
+  )
+};
 
 const TimelineEvent = ({ number, title, date, items }) => (
-  <div className="flex mb-8">
-    {/* Right side */}
+  <div className={`flex relative ${+number === 2 ? 'mt-8 -top-20' : (+number === 3 ? '-top-32' : '')
+    }`}>
     <div className="w-1/2 flex flex-col items-start gap-4 relative">
       <div className="absolute -right-32 top-5 text-[#AD9E68]/40 font-medium text-8xl">
         {number}
@@ -12,21 +38,24 @@ const TimelineEvent = ({ number, title, date, items }) => (
       <h3 className="text-[2.5rem] leading-[3.25rem] font-bold text-[#AD9E68] text-right text-balance">{title}</h3>
     </div>
     <Separator orientation='vertical' className='bg-[#AD9E68] w-[2px] h-auto mt-9' />
-    {/* Left side: Timeline items */}
     <div className="w-1/2 mt-[1.1rem] ms-[0.15rem]">
-      <div className="flex flex-col pb-8">
-        {items.map((item, index) => (
-          <div key={index} className="relative mb-8">
-            <div className="absolute -right-3 top-3 w-4 h-4 bg-[#AD9E68] rounded-full" />
-            <div className="px-8">
-              <h4 className="text-[#252C32] text-[1.375rem] leading-[2rem] text-right mb-8 text-balance font-bold">{item.title}</h4>
-              <div className="flex gap-8 items-center">
-                <div className='text-white rounded-full px-5 py-2 bg-gradient-to-r from-[#AD9E68] from-0% to-[#47412B] to-100% text-xs'>{item.type}</div>
-                <p className="text-lg text-[#AD9E68] text-right font-medium">{item.presenter}</p>
-              </div>
-            </div>
-          </div>
-        ))}
+      <div className={`${+number === 2 ? 'pb-32' : +number === 3 ? 'pb-0' : 'pb-16'} flex flex-col`}>
+        {items.length === 1 && items[0].isAccordion ? (
+          <Accordion type="single" collapsible className="w-full ms-8">
+            <AccordionItem value="item-1" className='bg-[#AD9E68]/20 rounded-2xl'>
+              <AccordionTrigger className="text-[#252C32] text-[1.375rem] leading-[2rem] text-right mb-4 text-balance font-bold hover:no-underline">
+                <TimelineItem {...items[0]} isFirstInAccordion={true} eventNumber={number} />
+              </AccordionTrigger>
+              <AccordionContent>
+                {items[0].subItems.map((subItem, index) => (
+                  <TimelineItem key={index} {...subItem} eventNumber={number} />
+                ))}
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+        ) : (
+          items.map((item, index) => <TimelineItem key={index} {...item} eventNumber={number} />)
+        )}
       </div>
     </div>
   </div>
@@ -39,17 +68,53 @@ export const ConferenceProgram = () => {
       title: "مهارات معاصرة في استدامة المشاريع",
       date: "12:00pm 10.08.2024",
       items: [
-        { title: "قواعد في إدارة مشاريع مستدامة", presenter: "د. طارق السويدان", type: "ورشة تدريبية" },
-        { title: "قوانين ومهارات في المؤسسية والقيادة بفعالية", presenter: "د. أكرم العدواني", type: "محاضرة تفاعلية" },
-        { title: "الريادة في السوق", presenter: "م. هيثم باكير", type: "ورشة تدريبية" },
+        { title: "قواعد في إدارة مشاريع مستدامة", presenter: "د. طارق السويدان", type: "دورة تدريبية" },
+        { title: "قوانين ومهارات في المؤسسية والقيادة بفعالية", presenter: "د. أكرم العدلوني", type: "محاضرة تدريبية" },
+        { title: "مهارات الريادة في السوق", presenter: "", type: "ورشة تدريبية" },
         { title: "مهارات الاستدامة وقياس الأثر الاجتماعي في القرن 21", presenter: "د. سامي العدواني", type: "ورشة تدريبية" },
+      ]
+    },
+    {
+      number: "02",
+      title: "ركائز وأخلاقيات ومفاهيم متقدمة",
+      date: "12:00pm 10.08.2024",
+      items: [
+        {
+          title: "الشروط الخمسة للمشاريع النهضوية",
+          presenter: "د. عبد الرزاق مقري",
+          type: "محاضرة تفاعلية",
+          isAccordion: true,
+          subItems: [
+            { title: "التحديات المعاصرة والنموذج الحضاري في الإسلام", presenter: "أ. وضاح خنفر", type: "ندوة حوارية" },
+            { title: "ركائز في إدارة المشاريع", presenter: "د. عبد الرزاق هزي", type: "ورش العمل" },
+          ]
+        },
+      ]
+    },
+    {
+      number: "03",
+      title: "قوة الإمكان الشبابي",
+      date: "12:00pm 10.08.2024",
+      items: [
+        {
+          title: "الإمكان الشبابي في بيئة الأعمال",
+          presenter: "أ. إبراهيم الهواري",
+          type: "محاضرة تدريبية",
+          isAccordion: true,
+          subItems: [
+            { title: "موقع المشاريع في الإعداد الحضاري وتغيير الواقع", presenter: "د. عبد الكريم بكار", type: "ندوة حوارية" },
+            { title: "10 مشاريع نهضوية رائدة في معالجة أزمات الأمّة الخمس", presenter: "منصة المشاريع", type: "العروض التقديمية" },
+            { title: "جائزة عمران السنوية ", presenter: "عُمران", type: "المُسابقة" },
+          ]
+        },
       ]
     },
   ];
 
   return (
-    <div className="py-16 px-4 md:px-8 lg:px-16 bg-white" dir="rtl">
+    <div className="pt-[8.5rem] bg-white" dir="rtl">
       <div className="max-w-5xl mx-auto">
+        <h1 className='text-center text-[2.5rem] leading-[3.25rem] font-bold mb-[8.5rem]'>برنامج المؤتمر</h1>
         {events.map((event, index) => (
           <TimelineEvent key={index} {...event} />
         ))}
--- components/ui/accordion.tsx
+++ components/ui/accordion.tsx
@@ -28,13 +28,13 @@ const AccordionTrigger = React.forwardRef<
     <AccordionPrimitive.Trigger
       ref={ref}
       className={cn(
-        "flex flex-1 items-center justify-between py-4 text-sm font-medium transition-all hover:underline [&[data-state=open]>svg]:rotate-180",
+        "flex flex-1 items-start justify-between py-4 text-sm font-medium transition-all hover:underline [&[data-state=open]>svg]:rotate-180",
         className
       )}
       {...props}
     >
       {children}
-      <ChevronDownIcon className="h-4 w-4 shrink-0 text-muted-foreground transition-transform duration-200" />
+      <ChevronDownIcon className="h-8 w-8 shrink-0 text-[#252C32] transition-transform duration-200 me-4" />
     </AccordionPrimitive.Trigger>
   </AccordionPrimitive.Header>
 ))
