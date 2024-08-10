b3d3ddcb753399bd6097a763b0a09d6262d0b25c
yamanidev <elamine.yamani@gmail.com>
2024-08-10 02:41:30 +0100

    Section made responsive

--- components/sections/why-annual-conference.tsx
+++ components/sections/why-annual-conference.tsx
@@ -31,9 +31,9 @@ const features = [
 type Feature = typeof features[number];
 
 const ConferenceFeature = ({ title, description, imageSrc }: Feature) => (
-  <div className="w-[296px] h-[242px] z-20">
+  <div>
     <Image src={imageSrc} alt={title} width={300} height={200} className="w-full object-cover rounded-[16px]" />
-    <div className="p-[22px] bg-white relative -top-6 rounded-[16px]">
+    <div className="p-5 lg:p-[1.375rem] relative bottom-8 bg-white rounded-2xl">
       <h3 className="text-[#252C32] text-base font-bold mb-4">{title}</h3>
       <p className="text-[#6B7280] text-[0.625rem] leading-[0.875rem] font-medium">{description}</p>
     </div>
@@ -42,17 +42,19 @@ const ConferenceFeature = ({ title, description, imageSrc }: Feature) => (
 
 export const WhyAnnualConference = () => {
   return (
-    <section className="bg-[#AD9E68] w-full h-[780px]">
-      <div className="pt-[98px] relative container mx-auto">
-        <div className="absolute top-36 left-5">
+    <section className="bg-[#AD9E68] w-full">
+      <div className="pt-[6.125rem] pb-[4.75rem] md:pb-[9.625rem] relative container mx-auto">
+        <div className="absolute left-5 z-10">
           <Image src={omranLogoSrc} alt="Omran Logo" width={290} height={246} quality={100} />
         </div>
         <div>
-          <h2 className="text-[2.5rem] leading-[3.25rem] font-bold mb-6 text-white">لماذا المؤتمر السنوي؟</h2>
-          <p className="text-base text-white">
-            تجمع المصممين حول العالم لنعالج إشكالية في مجال من مجالات حياة المسلمين، من خلال طرح المشاريع والمبادرات<br /> والتنافس حول جائزة عمران السنوية لأفضل مشروع يحقق الأثر المجتمعي المستدام.
-          </p>
-          <div className="mt-24 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+          <div className="relative z-10">
+            <h2 className="text-[2.5rem] leading-[3.25rem] font-bold text-white">لماذا المؤتمر السنوي؟</h2>
+            <p className="text-white">
+              تجمع المصممين حول العالم لنعالج إشكالية في مجال من مجالات حياة المسلمين، من خلال طرح المشاريع والمبادرات<br /> والتنافس حول جائزة عمران السنوية لأفضل مشروع يحقق الأثر المجتمعي المستدام.
+            </p>
+          </div>
+          <div className="mt-[7.5rem] md:mt-[6.25rem] grid grid-cols-1 md:grid-cols-[minmax(0,17rem)_minmax(0,17rem)] lg:grid-cols-4 gap-x-4 lg:gap-8 w-fit mx-auto">
             {features.map((feature) => (
               <ConferenceFeature key={feature.title} {...feature} />
             ))}
